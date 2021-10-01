28fa41b4e9663cc949e0fffbcf51057b20b390b6
TacosCyj <91713707+TacosCyj@users.noreply.github.com>
2021-10-01 21:04:09 +0800

    Update pre1.c

--- pre1.c
+++ pre1.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
 #include<ctype.h>
 #define LEN 1024
-#define null NULL
 
 char input_file[LEN];
-char read_in[LEN];
+char readfile[LEN];
 char ans[LEN];
 char* reserved[6] = {"If", "Else", "While", "Break", "Continue", "Return"};
 FILE* fp_r;
+
 
 int judgeIsReserve(char s[]){
 	int len, i, j;
@@ -29,156 +29,136 @@ int judgeIsReserve(char s[]){
 	return 6;
 }
 
-
-int lexicalAnalysis(int i){
-	char *pmove, title[LEN], temp_judge[LEN];
-	char *pp, temp_c;
-	int l, tp_l, len = 0, flag;
-	int ret = 0;
-	pmove = read_in;
-	while(len < i){
-		if(pmove == null) break;
-		if(*pmove == ' ' || *pmove == '\t'){
-			pmove++;
-			len++;
-		}
-		else if(*pmove == '\n') break;
+void lexicalAnalysis(){
+	char* pmove = readfile, *pp;
+	char ans_each[LEN], temp_judge[LEN];
+	int len_each = 0, len_temp, flag;
+	while(pmove != NULL){
+		if(*pmove == '\n') break;
+		if(*pmove == ' ' || *pmove == '\t') pmove++;
 		else if(isdigit(*pmove)){
-			strcpy(title, "Number");
-			strcat(title, "(");
-			l = strlen(title);
-			while(isdigit(*pmove)){
-				title[l++] = *pmove;
+			strcpy(ans_each, "Number(");
+			len_each = strlen(ans_each);
+			while(pmove != NULL && isdigit(*pmove)){
+				ans_each[len_each++] = *pmove;
 				pmove++;
-				len++;
-				if(pmove == null) break;
 			}
-			if(*pmove != ' ' && *pmove != '\n' && *pmove != '\t'){
-				pp = pmove;
-				if(*pp != ')' && *pp != ';'){
-					printf("Err");
-					ret = 1;
-					break;
-				}
-				else if(*pp == ';'){
-					if((pp + 1) != null){
-						if(*(pp + 1) != '\n'){
-							printf("Err");
-							ret = 1;
-							break;
-						}
-					}
-					else break;
-				}
+			if(pmove == NULL){
+				len_temp = strlen(ans_each);
+				ans_each[len_temp++] = ')';
+				ans_each[len_temp] = '\n';
+				strcat(ans, ans_each);
+				break;
 			}
-			strcat(title, ")");
-			tp_l = strlen(title);
-			title[tp_l] = '\n';
-			strcat(ans, title);
-			memset(title, 0, sizeof(title));
-		}
-		else if(isalpha(*pmove) || *pmove == '_'){
-			strcpy(title, "Ident");
-			strcat(title, "(");
-			l = strlen(title);
-			while(isalpha(*pmove) || *pmove == '_' || isdigit(*pmove)){
-				title[l++] = *pmove;
-				pmove++;
-				len++;
-				if(pmove == null) break;
-			}
-			if(*pmove != ' ' && *pmove != '\n' && *pmove != '\t'){
-				pp = pmove;
-				if(*pp != ')' && *pp != ';'){
-					printf("Err");
-					ret = 1;
-					break;
-				}
-				else if(*pp == ';'){
-					if((pp + 1) != null){
-						if(*(pp + 1) != '\n'){
-							printf("Err");
-							ret = 1;
-							break;
-						}
-					}
-					else break;
-				}	
-			}
-			flag = judgeIsReserve(title);
-			if(flag == 6){
-				strcat(title, ")");
-				tp_l = strlen(title);
-				title[tp_l] = '\n';
-				strcat(ans, title);
+			else if(*pmove != ' ' && *pmove != '\n' && *pmove != '\t' && *pmove != ')' && *pmove != ';'){
+				printf("Err");
+				exit(0);
 			}
 			else{
-				strcpy(temp_judge, reserved[flag]);
-				temp_judge[strlen(temp_judge)] = '\n';
-				strcat(ans, temp_judge);
-			}	
-			memset(title, 0, sizeof(title));
-			memset(temp_judge, 0, sizeof(temp_judge));
+				len_temp = strlen(ans_each);
+				ans_each[len_temp++] = ')';
+				ans_each[len_temp] = '\n';
+				strcat(ans, ans_each);
+				memset(ans_each, 0, sizeof(ans_each));
+			}
+		}
+		else if(isalpha(*pmove) || *pmove == '_'){
+			strcpy(ans_each, "Ident(");
+			len_each = strlen(ans_each);
+			while(pmove != NULL && (isalpha(*pmove) || *pmove == '_' || isdigit(*pmove))){
+				ans_each[len_each++] = *pmove;
+				pmove++;
+			}
+			if(pmove == NULL){
+				flag = judgeIsReserve(ans_each);
+				if(flag == 6){
+					strcat(ans_each, ")");
+					len_temp = strlen(ans_each);
+					ans_each[len_temp] = '\n';
+					strcat(ans, ans_each);
+				}
+				else{
+					strcpy(temp_judge, reserved[flag]);
+					temp_judge[strlen(temp_judge)] = '\n';
+					strcat(ans, temp_judge);
+				}	
+				break;
+			}
+			else if(*pmove != ' ' && *pmove != '\n' && *pmove != '\t' && *pmove != ')' && *pmove != ';'){
+				printf("Err");
+				exit(0);
+			}
+			else{
+				flag = judgeIsReserve(ans_each);
+				if(flag == 6){
+					strcat(ans_each, ")");
+					len_temp = strlen(ans_each);
+					ans_each[len_temp] = '\n';
+					strcat(ans, ans_each);
+				}
+				else{
+					strcpy(temp_judge, reserved[flag]);
+					temp_judge[strlen(temp_judge)] = '\n';
+					strcat(ans, temp_judge);
+				}	
+				memset(ans_each, 0, sizeof(ans_each));
+				memset(temp_judge, 0, sizeof(temp_judge));
+			}
 		}
 		else{
 			if(*pmove == '='){
 				char* pback = pmove;
 				int n = 0;
-				while(*pmove == '='){
+				while(pmove != NULL && *pmove == '='){
 					pback = pmove;
 					pmove++;
 					n++;
-					if(pmove == null) break;
 				}
 				pmove = pback;
 				if(n % 2 == 0){
 					n /= 2;
 					while(n--){
-						strcat(title, "Eq");
+						strcat(ans_each, "Eq");
 					}
 				}
 				else{
 					n -= 1;
 					n /= 2;
 					while(n--){
-						strcat(title, "Eq");
+						strcat(ans_each, "Eq");
 					}
-					strcat(title, "Assign");
+					strcat(ans_each, "Assign");
 				}
 			}
 			else{
 				switch(*pmove){
-					case ';': strcpy(title, "Semicolon"); break;
-					case '(': strcpy(title, "LPar"); break;
-					case ')': strcpy(title, "RPar"); break;
-					case '{': strcpy(title, "LBrace"); break;
-					case '}': strcpy(title, "RBrace"); break;
-					case '+': strcpy(title, "Plus"); break;
-					case '*': strcpy(title, "Mult"); break;
-					case '/': strcpy(title, "div"); break;
-					case '<': strcpy(title, "Lt"); break;
-					case '>': strcpy(title, "Gt"); break;
-					default: ret = 1; break;
+					case ';': strcpy(ans_each, "Semicolon"); break;
+					case '(': strcpy(ans_each, "LPar"); break;
+					case ')': strcpy(ans_each, "RPar"); break;
+					case '{': strcpy(ans_each, "LBrace"); break;
+					case '}': strcpy(ans_each, "RBrace"); break;
+					case '+': strcpy(ans_each, "Plus"); break;
+					case '*': strcpy(ans_each, "Mult"); break;
+					case '/': strcpy(ans_each, "div"); break;
+					case '<': strcpy(ans_each, "Lt"); break;
+					case '>': strcpy(ans_each, "Gt"); break;
+					default: printf("Err"); exit(0);
 				}
 			}
-			if((pmove + 1) != null){
-				temp_c = *(pmove + 1);
-				if(ret == 1 || (strcmp(title, "LPar") != 0 && temp_c != ' ' && temp_c != '\n' && temp_c != '\t')){
-					ret = 1;
+			if((pmove + 1) != NULL && strcmp(ans_each, "LPar") != 0){
+				char *pcheck = pmove + 1;
+				if(*pcheck != ' ' && *pcheck != '\t' && *pcheck != '\n'){
 					printf("Err");
-					break;
+					exit(0);
 				}
 			}
-			tp_l = strlen(title);
-			title[tp_l] = '\n';
-			strcat(ans, title);
-			memset(title, 0, sizeof(title));
+			len_temp = strlen(ans_each);
+			ans_each[len_temp] = '\n';
+			strcat(ans, ans_each);
+			memset(ans_each, 0, sizeof(ans_each));
 			pmove++;
-			len++;
 		}
-		
 	}
-	return ret;
 }
 
 int main(int argc, char* argv[]){
@@ -190,12 +170,8 @@ int main(int argc, char* argv[]){
 		scanf("%s", input_file);
 	}
 	fp_r = fopen(input_file, "r");
-	while(fgets(read_in, LEN, fp_r)){
-		i = strlen(read_in);
-		j = lexicalAnalysis(i);
-		if(j == 1){
-			break;
-		}
+	while(fgets(readfile, LEN, fp_r)){
+		lexicalAnalysis();
 		printf("%s", ans);
 		memset(ans, 0, sizeof(ans));
 	}
