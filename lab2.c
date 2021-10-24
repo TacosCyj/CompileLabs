9f6bf7bdb2261002b06a86343c607a4353185867
Cyj_Zym <91713707+TacosCyj@users.noreply.github.com>
2021-10-24 13:48:55 +0800

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -6,16 +6,35 @@
 
 char content[LEN];
 char token[LEN];
+int numstack[LEN];
+char OPTRstack[LEN];
+int opList[5] = {19, 20, 21, 22, 23};
+
+int top_n = -1;
+int top_o = -1;
 
 char* pmove;
+char* pmove_for_semi;
+char* pmove_exp;
 static int symbol;
 static int isLegal;
 static int returnValue;
+static int Value;
 static int numOfLBar;
 static int numOfRBar;
 static int numOfMinus;
 static int numOfPlus;
 
+int PriorityMatrix[9][9] = {
+    {1, 1, -1, -1, -1, -1, 1, 1},
+    {1, 1, -1, -1, -1, -1, 1, 1},
+    {1, 1, 1, 1, 1, -1, 1, 1},
+    {1, 1, 1, 1, 1, -1, 1, 1},
+    {1, 1, 1, 1, 1, -1, 1, 1},
+    {-1, -1, -1, -1, -1, -1, 0, -2},
+    {1, 1, 1, 1, 1, -2, 1, 1},
+    {-1, -1, -1, -1, -1, -1, -2, -2},
+};
 void isInt();
 void isMain();
 void isLBar();
@@ -28,7 +47,32 @@ void isRightExp();
 void isNumber();
 void isSemicolon();
 void isFinished();
+void initStack();
+void isExp();
 
+
+int numStack_top(){
+    return numstack[top_n];
+}
+int numStack_pop(){
+    return numstack[top_n--];
+}
+
+void numStack_push(int n){
+    numstack[++top_n] = n;
+}
+
+char optrStack_pop(){
+    return OPTRstack[top_o--];
+}
+
+char optrStack_top(){
+    return OPTRstack[top_o];
+}
+
+void optrStack_push(char c){
+    OPTRstack[++top_o] = c;
+}
 
 void reverse(){
     int i = 0, j = strlen(token) - 1;
@@ -167,7 +211,13 @@ void getsym(){
                 getsym();
             }
         }
-        else symbol = -1;
+        else{
+            token[0] = '/';
+            pmove_exp = pmove;
+            //printf("%c", *pmove_exp);
+            symbol = 22;
+            pmove++;
+        }
     }
     else if(isdigit(*pmove)){
         while(isdigit(*pmove) || isalpha(*pmove)){
@@ -193,22 +243,26 @@ void getsym(){
             case ')': token[0] = ')'; symbol = 15; break;
             case '{': token[0] = '{'; symbol = 16; break;
             case '}': token[0] = '}'; symbol = 17; break;
-            case ';': token[0] = ';'; symbol = 18; break;
+            case ';': token[0] = ';'; symbol = 18; pmove_for_semi = pmove; break;
             case '+': token[0] = '+'; symbol = 19; break;
             case '-': token[0] = '-'; symbol = 20; break;
+            case '*': token[0] = '*'; symbol = 21; break;
+            case '/': token[0] = '/'; symbol = 22; break;
+            case '%': token[0] = '%'; symbol = 23; break;
             default: symbol = -1; break;
         }
+        pmove_exp = pmove;
         pmove++;
     }
 }
 
-void getReturnValue(){
+void getValue(){
     int i;
     int l = strlen(token);
+    Value = 0;
     for(i = 0; i < l; i++){
-        returnValue = returnValue * 10 + (token[i] - '0');
+        Value = Value * 10 + (token[i] - '0');
     }
-    if(numOfMinus % 2) returnValue *= -1;
 }
 
 void init(){
@@ -255,57 +309,183 @@ void isLBrace(){
     else isLegal = 1;
 }
 
+void initStack(){
+    optrStack_push(';');
+}
+
 void isReturn(){
     getsym();
     if(symbol == 13){
-        isLeftExp();
+        initStack();
+        isExp();
     }
     else isLegal = 1;
 }
 
-void isLeftExp(){
-    getsym();
-    if(symbol == 14){
-        numOfLBar++;
-        isLeftExp();
+int calculate(){
+    if(top_n == 0 && top_o  > 0){
+        int a = numStack_pop();
+        int b = 0;
+        char op = optrStack_top();
+        switch(op){
+            case '+': numStack_push(b + a); break;
+            case '-': numStack_push(b - a); break;
+            case '*': numStack_push(b * a); break;
+            case '/': numStack_push(b / a); break;
+            case '%': numStack_push(b % a); break;
+            default: break;
+        }
+        optrStack_pop();
+        return 0;
     }
-    else if(symbol == 19){
-        numOfPlus++;
-        isLeftExp();
-    }
-    else if(symbol == 20){
-        numOfMinus++;
-        isLeftExp();
-    }
-    else if(symbol == 10){
-        isNumber();
-    }
-    else isLegal = 1;
-}
-
-void isRightExp(){
-    if(numOfLBar == 0 || numOfLBar == numOfRBar){
-        isSemicolon();
+   else if(top_n > 0){
+        int a = numStack_pop();
+        int b = numStack_pop();
+        char op = optrStack_top();
+        switch(op){
+            case '+': numStack_push(b + a); break;
+            case '-': numStack_push(b - a); break;
+            case '*': numStack_push(b * a); break;
+            case '/': numStack_push(b / a); break;
+            case '%': numStack_push(b % a); break;
+            default: break;
+        }
+        optrStack_pop();
+        return 0;
     }
     else{
+        return 1;
+    }
+}
+
+int getPriority(char a, char b){
+    char c_x = a;
+    char c_y = b;
+    int x, y;
+    switch(c_x){
+        case '+': x = 0; break;
+        case '-': x = 1; break;
+        case '*': x = 2; break;
+        case '/': x = 3; break;
+        case '%': x = 4; break;
+        case '(': x = 5; break;
+        case ')': x = 6; break;
+        case ';': x = 7; break;
+        default: break;
+    }
+    switch(c_y){
+        case '+': y = 0; break;
+        case '-': y = 1; break;
+        case '*': y = 2; break;
+        case '/': y = 3; break;
+        case '%': y = 4; break;
+        case '(': y = 5; break;
+        case ')': y = 6; break;
+        case ';': y = 7; break;
+        default: break;
+    }
+    return PriorityMatrix[y][x];
+}
+
+int ScannerFurtherForNum(){
+    int num_of_num = 0;
+    while(*pmove_exp != ')'){
+        if(*pmove_exp == ' ' || *pmove_exp == '\n' || *pmove_exp == '\t' || *pmove_exp == '\r') pmove_exp++;
+        if(isdigit(*pmove_exp)){
+            while(isdigit(*pmove_exp) || isalpha(*pmove_exp)){
+                pmove_exp++;
+            }
+            num_of_num++;
+        }
+        else pmove_exp++;
+    }
+    return num_of_num;
+}
+
+int ScannerFurtherForOp(){
+    int num_of_op = 0;
+    //printf("%c", *pmove_exp);
+    while(*pmove_exp == '*' || *pmove_exp == '/' || *pmove_exp == ' '){
+        if(*pmove_exp == '*' || *pmove_exp == '/'){
+            num_of_op++;
+            pmove_exp++;
+        }
+        else pmove_exp++;
+    }
+    return num_of_op;
+}
+
+int ScannerFurtherForMinusNum(){
+    pmove_exp++;
+    if(isdigit(*pmove_exp)) return 0;
+    else return 1;
+}
+
+void isExp(){
+    int flag = 1;
+    int is_Minus = 1;
+    while(*pmove != '\n' && *pmove != '\r'){
         getsym();
-        if(symbol == 15){
-            numOfRBar++;
-            isRightExp();
+        if(symbol == 10){
+            getValue();
+            if(is_Minus == 0) Value *= -1;
+            //printf("%d\n", Value);
+            numStack_push(Value);
+            is_Minus = 1;
+        }
+        else if((symbol >= 19 && symbol <= 23) || symbol == 14 || symbol == 15){
+            if(symbol == 21 || symbol == 22){
+                int tt = ScannerFurtherForOp();
+                //printf("%d", tt);
+                if(tt != 1) break;
+            }
+            if(symbol == 20){
+                int ttt = ScannerFurtherForMinusNum();
+                if(ttt == 0){
+                    is_Minus = 0;
+                    continue;
+                }
+            }
+            int j = getPriority(token[0], optrStack_top());
+            if(j == -1){
+                optrStack_push(token[0]);
+            }
+            else if(j == 1){
+                while(getPriority(token[0], optrStack_top()) == 1){
+                    int f = calculate();
+                    if(f == 1) break;
+                }
+                if(optrStack_top() == '('){
+                optrStack_pop();
+                }
+                if(token[0] != ')') optrStack_push(token[0]);
+            }
+            else if(j == 0){
+                optrStack_pop();
+            }
+            else if(j == -2){
+                isLegal = 1;
+                return;
+            }
+        }
+        else if(symbol == 18){
+            while(getPriority(token[0], optrStack_top()) == 1){
+                calculate();
+            }
+            if(top_n == 0 && top_o == 0 && optrStack_top() == ';'){
+                pmove = pmove_for_semi;
+                flag = 0;
+                break;
+            }
+            else isLegal = 1;
         }
         else isLegal = 1;
     }
-}
+    if(flag == 0) isSemicolon();
+    else isLegal = 1;
+ }
 
-void isNumber(){
-    //getsym();
-    getReturnValue();
-       //isSemicolon();
-    isRightExp();
-}
-
-
-void isSemicolon(){
+ void isSemicolon(){
     getsym();
     if(symbol == 18){
         isRBrace();
@@ -331,6 +511,7 @@ void isFinished(){
 }
 
 void PrintCode(){
+    returnValue = numStack_top();
     printf("define dso_local i32 @main(){\n");
     printf("    ret i32 %d\n", returnValue);
     printf("}");
