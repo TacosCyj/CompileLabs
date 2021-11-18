9e03c994c11ce048e550fd4635aba88a25d6afaf
Cyj_Zym <91713707+TacosCyj@users.noreply.github.com>
2021-11-18 15:59:56 +0800

    Update Analysis.java

--- src/Analysis.java
+++ src/Analysis.java
@@ -8,35 +8,28 @@ public class Analysis {
         StringBuilder input_file = new StringBuilder();
         StringBuilder output_file = new StringBuilder();
         LinkedList<token> tokenlist;
-        input_file.append("E:\\编译原理\\lab3\\testfile\\a.txt");
-        output_file.append("E:\\编译原理\\lab3\\testfile\\b.txt");
+        input_file.append(args[0]);
+        output_file.append(args[1]);
         Lexer lexer = Lexer.getLexerInstance();
         lexer.setFile(input_file);
         lexer.getContent();
-        if(lexer.lexerAnalysis()){
-            tokenlist = lexer.getTokenList();
-            operator o = new operator('#', "Op", 28);
-            tokenlist.offer(o);
-            Grammar grammar = Grammar.getInstance();
-            expression exp = expression.getInstance();
-            grammar.setTokenList(tokenlist);
-            grammar.setExper(exp);
-            boolean flag = grammar.isInt();
-            if(flag){
-                try{
-                    FileWriter writer = new FileWriter(String.valueOf(output_file));
-                    writer.write(grammar.getAnswer().toString());
-                    writer.flush();
-                    writer.close();
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-                System.exit(0);
-            }
-            else System.exit(1);
-        }
-        else{
-            System.exit(1);
-        }
+        lexer.lexerAnalysis();
+        tokenlist = lexer.getTokenList();
+        operator o = new operator('#', "Op", 28);
+        tokenlist.offer(o);
+        Grammar grammar = Grammar.getInstance();
+        expression exp = expression.getInstance();
+        grammar.setTokenList(tokenlist);
+        grammar.setExper(exp);
+        boolean flag = grammar.isInt();
+        try{
+               FileWriter writer = new FileWriter(String.valueOf(output_file));
+               writer.write(grammar.getAnswer().toString());
+               writer.flush();
+               writer.close();
+               }catch(IOException e){
+                     e.printStackTrace();
+               }
+               System.exit(0);
     }
 }
