c8c46d1bdc72e2a6658d841e86c425eecbf4d2e1
Cyj_Zym <91713707+TacosCyj@users.noreply.github.com>
2021-11-27 16:09:36 +0800

    Delete operator.java

--- src/operator.java
+++
@@ -1,8 +0,0 @@
-public class operator extends token{
-    private String operator;
-    public operator(String c, String t, int s){
-        super(t, s);
-        this.operator = c;
-    }
-    public String getOperator(){return this.operator;}
-}
