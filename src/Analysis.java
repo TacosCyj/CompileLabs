95d00624a3bda11e936a32ab291f0e46db4602ec
Cyj_Zym <91713707+TacosCyj@users.noreply.github.com>
2021-12-01 11:17:28 +0800

    Update Analysis.java

--- src/Analysis.java
+++ src/Analysis.java
@@ -13,8 +13,6 @@ public class Analysis {
         Lexer lexer = Lexer.getLexerInstance();
         lexer.setFile(input_file);
         lexer.getContent();
-        System.out.println( lexer.getcon().toString());
-        System.exit(0);
         if(lexer.lexerAnalysis()){
             tokenlist = lexer.getTokenList();
             operator o = new operator("#", "Op", 28);
